64d26861a2f4462395aee0205d9a7aefe36b587f
Boyan-Yordanov <boyanyordanov00@gmail.com>
2023-12-04 22:29:36 +0000

    adding Day 2 and an example dir

---
+++ example/task_1.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+
+def main():
+    ans = 0        
+    puzzle = []
+    
+    with open("input.txt") as my_file:
+        for line in my_file:
+            puzzle.append(line)
+            
+            
+    return ans
+
+
+if __name__ == "__main__":
+    print(main())
